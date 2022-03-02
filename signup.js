32a511c123d8b3294427c116ad5dd41a35383081
Juan Antonio León <jalojr@hotmail.com>
2022-03-02 19:49:49 +0000

    repeat password done

--- signup.js
+++ signup.js
@@ -3,45 +3,55 @@ Vue.component('form-template', {
     return {
       email: '',
       password: '',
+      repPassword: '',
       wrongEmail: false,
       wrongPass: false,
+      repPass: false,
       canSubmit: false
     }
   },
   methods: {
-    checkEmail(email) {
-      if (!(/^(\w+)@(\w+)\.(\w\w+)$/).test(email) && email.length !== 0) {
+    checkEmail() {
+      if (!(/^(\w+)@(\w+)\.(\w\w+)$/).test(this.email) && this.email.length !== 0) {
         this.wrongEmail = true
       } else {
         this.wrongEmail = false
       }
       this.allOk()
     },
-    checkPassword(password) {
-      if (password.length < 5 && password.length !== 0) {
+    checkPassword() {
+      if (this.password.length < 5 && this.password.length !== 0) {
         this.wrongPass = true
       } else {
         this.wrongPass = false
       }
       this.allOk()
     },
-    requestAccess(canSubmit) {
-      canSubmit ? alert('You are IN!') : alert('Access Denied')
+    repeatPassword() {
+      this.repPassword !== this.password ?  this.repPass = true : this.repPass = false
+      this.allOk()
+    },
+    requestAccess() {
+      this.canSubmit ? alert('You are IN!') : alert('Access Denied')
     },
     allOk() {
-      if(this.email.length === 0 || this.password.length === 0) {
+      if(this.email.length === 0 || this.password.length === 0 || this.repPassword.length === 0) {
         return
       }
-      if (!this.wrongEmail && !this.wrongPass) {
+      if (!this.wrongEmail && !this.wrongPass && !this.repPass) {
         this.canSubmit = true
+      } else {
+        this.canSubmit = false
       }
     }
   },
   template: `
   <section class="access">
-    <input v-model.trim="email" :class="{'wrong': wrongEmail}" type="email" placeholder="Insert email" @keyup="checkEmail(email)">
+    <input v-model.trim="email" :class="{'wrong': wrongEmail}" type="email" placeholder="Insert email" @keyup="checkEmail()">
     <br>
-    <input v-model="password" :class="{'wrong': wrongPass}" type="password" placeholder="Insert password" @keyup="checkPassword(password)">
+    <input v-model="password" :class="{'wrong': wrongPass}" type="password" placeholder="Insert password" @keyup="checkPassword()">
+    <br>
+    <input v-model="repPassword" :class="{'wrong': repPass}" type="password" placeholder="Repeat password" @keyup="repeatPassword()">
     <br>
     <button @click="requestAccess(canSubmit)" :class="{'isActive': canSubmit}">Sign Up</button>
   </section>`
